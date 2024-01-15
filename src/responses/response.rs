c86b6eacc36d461a6dd7e32377d0e5bf7441eedd
Camille Nevermind <38858480+nevermille@users.noreply.github.com>
2024-01-15 09:40:06 +0100

    :memo: Fix documentation errors

--- src/responses/response.rs
+++ src/responses/response.rs
@@ -15,6 +15,7 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#[derive(Default)]
 /// A response from Mailjet
 pub struct Response<T> {
     /// The HTTP Response code
